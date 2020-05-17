13f5c0280c088963b0545903cc028af5baa67814
iantheninja <munene.ian@live.com>
2020-05-18 02:30:38 +0300

    click listeners for div buttons functional

---
+++ app.js
@@ -0,0 +1,23 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+
+rock_div.addEventListener('click', function(){
+    console.log("hello there rock");
+  });
+
+
+paper_div.addEventListener('click', function(){
+  console.log("hello there paper");
+});
+
+scissors_div.addEventListener('click', function(){
+  console.log("hello there scissors");
+});
