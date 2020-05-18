c1763580af5b5e450d5bf368ce81579dcdd05adf
iantheninja <munene.ian@live.com>
2020-05-18 03:25:48 +0300

    js logic functional, comp and user scores good

--- app.js
+++ app.js
@@ -8,16 +8,54 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+function draw() {
+  console.log("The round is a draw");
+}
+
+function win() {
+  console.log("You won this round!! Congratulations!!!");
+}
+
+function lose() {
+  console.log("You lost the round! Try again!");
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+
+  if (computerChoice == userChoice) {
+    draw();
+  } else if (computerChoice === "rock" && userChoice === "paper"){
+    win();
+  } else if  (computerChoice === "paper" && userChoice === "scissors") {
+    win();
+  } else if  (computerChoice === "rock" && userChoice === "scissors") {
+    lose();
+  } else if  (computerChoice === "scissors" && userChoice === "paper") {
+    lose();
+  } else if  (computerChoice === "paper" && userChoice === "rock") {
+    lose();
+  } else if  (computerChoice === "scissors" && userChoice === "rock") {
+    win ();
+  } else {
+    console.log("Invalid choice try again");
+  }
+}
 
 rock_div.addEventListener('click', function(){
-    console.log("hello there rock");
-  });
+  game("rock");
+});
 
 
 paper_div.addEventListener('click', function(){
-  console.log("hello there paper");
+  game("paper");
 });
 
 scissors_div.addEventListener('click', function(){
-  console.log("hello there scissors");
+  game("scissors");
 });
