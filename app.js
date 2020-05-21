8b9809751af3f4fc4c5f2baa43a3d19f0473f0d7
iantheninja <munene.ian@live.com>
2020-05-21 16:59:23 +0300

    user and computer scores getting updated correctly

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".score-board");
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.querySelector("#user-score");
+const computerScore_span = document.querySelector("#comp-score");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+const actionMessage_div = document.getElementById("action-message");
+
 
 
 function getComputerChoice() {
@@ -14,20 +15,35 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
+
 function draw(compChoice, usrChoice) {
   result_div.innerHTML = "<p>You chose <span id='draw'>" + usrChoice + "</span>. Computer Chose <span id='draw'>" + compChoice + "</span>.</p>";
-  console.log("The round is a draw");
+
+  actionMessage_div.innerHTML = "The round is a draw, try again.";
 }
+
 
 function win(compChoice, usrChoice) {
+  userScore += 1;
+
+  userScore_span.innerText = userScore;
+
   result_div.innerHTML = "<p>You chose <span id='win'>" + usrChoice + "</span>. Computer Chose <span id='lose'>" + compChoice + "</span>.</p>";
-  console.log("You won this round!! Congratulations!!!");
+
+  actionMessage_div.innerHTML = "You won this round!! Congratulations!!!"
 }
 
+
 function lose(compChoice, usrChoice) {
+  computerScore += 1;
+
+  computerScore_span.innerText = computerScore; 
+  
   result_div.innerHTML = "<p>You chose <span id='lose'>" + usrChoice + "</span>. Computer Chose <span id='win'>" + compChoice + "</span>.</p>";
-  console.log("You lost the round! Try again!");
+
+  actionMessage_div.innerHTML = "You lost this time, Try Again!"
 }
+
 
 function game(userChoice) {
   const computerChoice = getComputerChoice();
@@ -59,6 +75,7 @@ rock_div.addEventListener('click', function(){
 paper_div.addEventListener('click', function(){
   game("paper");
 });
+
 
 scissors_div.addEventListener('click', function(){
   game("scissors");
